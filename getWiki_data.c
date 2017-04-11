d3ea870a5063376e89febbc2f882dd22e54be454
Larry Li <li_j8@denison.edu>
2017-04-11 16:41:43 -0400

    add comment

--- getWiki_data.c
+++ getWiki_data.c
@@ -1,9 +1,12 @@
+// Description: This file works for downloading the data from wikimedia traffic data by using MPI
+
 #include <stdlib.h>
 
 int main() {
     int year;
     int month;
     int day;
+
 
     char *FileName = malloc
     system("wget -nc --directory-prefix=./ https://dumps.wikimedia.org/other/pagecounts-raw/2015/2015-01/pagecounts-20150101-010000.gz");
