3c2aaedfcf64e123947079ab2efd56f03cc4b5a7
Nico <nvaldiviezon@gmail.com>
2017-04-11 16:38:31 -0400

    hello

---
+++ getWiki_data.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+int main() {
+    int year;
+    int month;
+    int day;
+
+    char *FileName = malloc
+    system("wget -nc --directory-prefix=./ https://dumps.wikimedia.org/other/pagecounts-raw/2015/2015-01/pagecounts-20150101-010000.gz");
+    system("gzip -d ")
+    return 0;
+}
