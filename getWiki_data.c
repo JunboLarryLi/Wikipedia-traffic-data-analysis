ff1ca49353f1b1ef3379c60f73910a4c301d3e9e
Nico <nvaldiviezon@gmail.com>
2017-04-12 01:21:28 -0400

    execute command line

--- getWiki_data.c
+++ getWiki_data.c
@@ -3,28 +3,58 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-
-char* concat(const char *s1, const char *s2)
-{
-    const int len1 = strlen(s1);
-    const int len2 = strlen(s2);
-    char *result = malloc(len1+len2+1);//+1 for the zero-terminator
-    //in real code you would check for errors in malloc here
-    memcpy(result, s1, len1);
-    memcpy(result+len1, s2, len2+1);//+1 to copy the null-terminator
-    return result;
-}
+#include <time.h>
 
 int main() {
-    char *listOfYears = {"2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016"};
-    //account for leap years, months with 30/31/28/29 days
-    char *listOfMonths = {"01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"};
-    //char *listOfDays = {"01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "10", "11", "12", "10", "11", "12", "10", "11", "12", "10", "11", "12", "10", "11", "12"}
-    char *date = concat(listOfYears[1], listOfMonths[5]);
-    //system("wget -nc --directory-prefix=./ https://dumps.wikimedia.org/other/pagecounts-raw/2015/2015-01/pagecounts-20150101-010000.gz");
-    //system("gzip -d ")
+    //start at 0153.11.9  --> 2007/12/09
+    //2015   --> 0163.0.1
+    //end at 0153.11.3171   -->2016/08/05
+    char date[9];
+    time_t t = time(NULL);
+    struct tm * ptm = gmtime(&t);
+    char year[4];
+    char month[2];
+    char nameOfGZ[26];
+    char URL_forDay[82];
+    char Download_gz_URL[100];
+    char wgetCommand[154];
+    char unZipCommand[114];
+    char RemoveCommand[114];
 
-    printf("%s\n",date);
 
+
+    for (int i = 1; i < 584; i++) {
+      ptm->tm_year = 0163;
+      ptm->tm_mon = 0;
+      ptm->tm_mday = i;
+      mktime(ptm);
+      strftime(date, 9, "%Y%m%d", ptm);
+
+      memcpy(&year, &date[0], 4);
+      year[4] = '\0';
+      memcpy(&month, &date[4], 2);
+      month[2] = '\0';
+
+      snprintf(URL_forDay, 82, "https://dumps.wikimedia.org/other/pagecounts-raw/%s/%s-%s/pagecounts-%s", year, year, month, date);
+      for (int j = 0; j < 24; j++) {
+        if (j < 10) {
+          snprintf(Download_gz_URL, 100, "%s-0%d0000.gz", URL_forDay, j);
+        }
+        else {
+          snprintf(Download_gz_URL, 100, "%s-%d0000.gz", URL_forDay, j);
+        }
+        memcpy(&nameOfGZ, &Download_gz_URL[62], 26);
+        nameOfGZ[26] = '\0';
+
+
+        snprintf(wgetCommand, 154, "wget %s |hdfs dfs -put %s.gz ./WikiData/", Download_gz_URL, nameOfGZ);
+        snprintf(unZipCommand, 114, "hadoop fs -text ./WikiData/%s.gz | hadoop fs -put - ./WikiData/%s", nameOfGZ, nameOfGZ);
+
+        system(wgetCommand);
+        system(unZipCommand);
+      }
+      system("hdfs dfs -rm ./Wikidata/*.gz");
+
+    }
     return 0;
 }
