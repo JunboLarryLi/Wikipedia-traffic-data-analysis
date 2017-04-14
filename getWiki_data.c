82d5cdf74b1b6ed35e985fa4970b1616b79459dc
Nico <nvaldiviezon@gmail.com>
2017-04-14 01:17:50 -0400

    final code downloading from 2015-01-17 000000 until 2015-04-30 230000

--- getWiki_data.c
+++ getWiki_data.c
@@ -14,7 +14,7 @@
 int nnodes;
 int me; // node's rank
 int recv_buff[3];
-
+int cmd;
 void init(int argc, char **argv) {
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &nnodes);
@@ -23,44 +23,39 @@ void init(int argc, char **argv) {
 
 void managernode_DistWork() {
   MPI_Status status;
-  int i;
-
+  int i, days;
+  days = 104;
   //dividing data among all the nodes except managernode
   //sub_size[0] = size
   //sub_size[1] = start number
   //sub_size[1] = end number
-  // int* sub_size;
-  // sub_size = malloc(3*sizeof(int));
   int sub_size[3];
-  sub_size[0] = 584/(nnodes-1);
+  sub_size[0] = days/(nnodes-1);
   sub_size[1] = 0;
   sub_size[2] = 0;
 
-  // int* last_sub_size;
-  // last_sub_size = malloc(3*sizeof(int));
   int last_sub_size[3];
-  last_sub_size[0] = sub_size[0] + (584 - sub_size[0]*(nnodes-1));
-  // printf("caooooooooooooooo %d\n", last_sub_size[0]);
+  last_sub_size[0] = sub_size[0] + (days - sub_size[0]*(nnodes-1));
   last_sub_size[1] = 0;
   last_sub_size[2] = 0;
-  // printf("Rank: %d: list looks like: %d, %d, %d \n", me, sub_size[0], sub_size[1], sub_size[2]);
   // send share of the downloading dates to worker nodes
-  for (i = 1; i < nnodes-1; i++)
-  {
+  for (i = 1; i < nnodes-1; i++) {
     sub_size[1] = (i-1)*sub_size[0]+1;
     sub_size[2] = i*sub_size[0];
-    // printf("Rank: %d: list looks like: %d, %d, %d \n", me, sub_size[0], sub_size[1], sub_size[2]);
     MPI_Send (&sub_size, 3, MPI_INT, i, DATA_MSG, MPI_COMM_WORLD);
   }
+
   last_sub_size[1] = (i-1)*sub_size[0]+1;
   last_sub_size[2] = last_sub_size[1] + last_sub_size[0]-1;
-  // printf("Rank: %d: list looks like: %d, %d, %d \n", me, last_sub_size[0], last_sub_size[1], last_sub_size[2]);
   MPI_Send (&last_sub_size, 3, MPI_INT, i, DATA_MSG, MPI_COMM_WORLD);
 
 }
 
 void MasterPutHDFS() {
   MPI_Status status;
+  char slaveToMaster[74];
+  char rmSlaveTmpData[35];
+
   const char *a[17];
   a[0]="219a";
   a[1]="219b";
@@ -81,13 +76,26 @@ void MasterPutHDFS() {
   a[16]="216f";
 
   int i;
-  //system("mkdir /tmp/WikiData");
+  cmd = system("mkdir /tmp/Wiki_Data");
   //forloop from 1-16 missing 17
   for (i = 0; i < nnodes; i++) {
-      printf("HELLOO this is %s\n", a[i]);/*
-      system("scp -r valdiv_n1@219a.mathsci.denison.edu:/tmp/WikiData/ /tmp/WikiData/");
-      system("hdfs dfs -put /tmp/WikiData/ .");*/
+    snprintf(slaveToMaster, 73, "scp -r %s:/tmp/WikiData/ /tmp/Wiki_Data/", a[i]);
+    snprintf(rmSlaveTmpData, 35, "ssh %s 'rm -r /tmp/WikiData/'", a[i]);
+
+    printf("HELLOO this is %s.... Moving data to Hadoop2\n", a[i]);
+    cmd = system(slaveToMaster);    //copy data to Hadoop2
+
+    printf("Removing from Slave......\n");
+    cmd = system(rmSlaveTmpData);   //erase data from slave
+
+    printf("Moving from Hadoop to HDFS......\n");
+    cmd = system("hdfs dfs -put /tmp/Wiki_Data/WikiData ./Wiki_Data/");  //move data to HDFS
+
+    printf("Removing Data from Hadoop......\n");
+    cmd = system("rm -r /tmp/Wiki_Data/WikiData");
+
   }
+  cmd = system("rm -r /tmp/Wiki_Data");
 }
 
 void workernode()
@@ -95,99 +103,39 @@ void workernode()
   // int* sub_size; // a buffer that receives the data
   // sub_size = malloc(3*sizeof(int));
   MPI_Status status;
-  int cmd;
-  //char file[30];
+
+  int cmd, start, end, i, j;
+  char date[9];
+  char year[4];
+  char month[2];
+  //start at 0153.11.9  --> 2007/12/09
+  //2015   --> 0163.0.1
+  //end at 0153.11.3171   -->2016/08/05
+  time_t t = time(NULL);
+  struct tm * ptm = gmtime(&t);
+
+  //char nameOfGZ[26];
+  char URL_forDay[82];
+  char Download_gz_URL[100];
+  char MakeDir[28];
+  char wgetCommand[122];
+
   //reeive message from manager node
   MPI_Recv(&recv_buff, 3, MPI_INT, 0, DATA_MSG, MPI_COMM_WORLD, &status);
+  if (me != 1) {
+    start = recv_buff[1];
+    end = recv_buff[2];
 
-  int start = recv_buff[1];
-  int end = recv_buff[2];
+  //  printf("Rank: %d: list looks like: %d, %d, %d\n ", me, recv_buff[0], start, end);
+    cmd = system("mkdir /tmp/WikiData");  //Create WikiData directory
 
-  printf("Rank: %d: list looks like: %d, %d, %d\n ", me, recv_buff[0], start, end);
-/*  if (me == 2) {
-    system("wget https://dumps.wikimedia.org/other/pagecounts-raw/2007/2007-12/pagecounts-20071209-180000.gz -P /tmp/WikiData/");
-    system("wget https://dumps.wikimedia.org/other/pagecounts-raw/2007/2007-12/pagecounts-20071209-190000.gz -P /tmp/WikiData/");
-
-  }
-*/
-  //do work
-  /*
-  write
-  code
-  from
-  here
-  */
-  // float sub_pi = calculate_pi(sub_size);
-
-  //send result back to manager node
-  // MPI_Send(&sub_pi, 1, MPI_INT, 0, NEWDATA_MSG, MPI_COMM_WORLD);
-}
-
-int main(int argc, char** argv)
-{
-  int i;
-  init(argc, argv);
-  // printf("rank: %d\n", me);
-  if (me == 0)
-  {
-    printf("rankkkkkkkkkkkkk: %d\n", me);
-    managernode_DistWork();
-  }
-  else
-  {
-    workernode();
-  }
-
-  MPI_Barrier(MPI_COMM_WORLD);
-
-  if (me == 0) {
-    MasterPutHDFS();
-  }
-
-  MPI_Finalize();
-}
-
-/*
-// Description: This file works for downloading the data from wikimedia traffic data by using MPI
-//mpicc -g -o getWiki_data getWiki_data.c
-//mpiexec -f hosts -n 16 ./getWiki_data
-
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include <time.h>
-//#include <mpi.h>
-
-int main() {
-    //start at 0153.11.9  --> 2007/12/09
-    //2015   --> 0163.0.1
-    //end at 0153.11.3171   -->2016/08/05
-    char date[9];
-    time_t t = time(NULL);
-    struct tm * ptm = gmtime(&t);
-
-    char year[4];
-    char month[2];
-
-    char nameOfGZ[26];
-    char URL_forDay[82];
-    char Download_gz_URL[100];
-
-    char MakeDir[28];
-    char wgetCommand[122];
-    //char unZipCommand[114];
-    //char RemoveCommand[114];
-
-    int start = 2;
-    int end = 3;
-
-    system("mkdir /tmp/WikiData");  //Create WikiData directory
     //584
-    for (int i = start; i < end; i++)
+    //first run 17, second run 104
+    for (i = start; i <= end; i++)
     {
       ptm->tm_year = 0163;
       ptm->tm_mon = 0;
-      ptm->tm_mday = i;
+      ptm->tm_mday = 16 + i;
       mktime(ptm);
       strftime(date, 9, "%Y%m%d", ptm);
       memcpy(&year, &date[0], 4);
@@ -197,34 +145,43 @@ int main() {
       snprintf(URL_forDay, 82, "https://dumps.wikimedia.org/other/pagecounts-raw/%s/%s-%s/pagecounts-%s", year, year, month, date);
       snprintf(MakeDir, 28, "mkdir /tmp/WikiData/%s-%s", year, month);
 
-      system(MakeDir);
-
-      printf("%s\n", URL_forDay);
-      for (int j = 0; j < 5; j++) {
+      cmd = system(MakeDir);
+      for (j = 0; j < 24; j++) {
         if (j < 10) {
           snprintf(Download_gz_URL, 100, "%s-0%d0000.gz", URL_forDay, j);
         }
         else {
           snprintf(Download_gz_URL, 100, "%s-%d0000.gz", URL_forDay, j);
         }
-        memcpy(&nameOfGZ, &Download_gz_URL[62], 26);
-        nameOfGZ[26] = '\0';  //get name of the
-
-        snprintf(wgetCommand, 154, "wget %s -P /tmp/WikiData/%s-%s", Download_gz_URL, year, month);
-        system(wgetCommand);
+        snprintf(wgetCommand, 122, "wget %s -P /tmp/WikiData/%s-%s", Download_gz_URL, year, month);
+        cmd = system(wgetCommand);
 
       }
-
-
-      //for slave nodes to run HADOOP DOESN'T RUN THESE COMMANDS
-      //snprintf(unZipCommand, 114, "hadoop fs -text ./WikiData/%s.gz | hadoop fs -put - ./WikiData/%s", nameOfGZ, nameOfGZ);
-      //system(unZipCommand);
     }
-    //system("scp -r /tmp/WikiData/ valdiv_n1@hadoop2.mathsci.denison.edu:/users/valdiv_n1/CS345/Hadoop/Wiki_Data");
-    //system("rm -r /tmp/WikiData/")
-    //MASTER in a separete forloop
-    // if rank == 0
-    // system("hdfs dfs -put ~/CS345/Hadoop/Wiki_Data/ .")
-    return 0;
+  }
 }
-*/
+
+int main(int argc, char** argv)
+{
+  int i;
+  init(argc, argv);
+  /*
+  master: Distribute file range.
+  slaves: Get files
+  */
+  if (me == 0) {
+    printf("I am the Master of rank: %d\n", me);
+    managernode_DistWork();
+  } else {
+    workernode();
+  }
+  /*BARRIER ==> all documents have been downloaded*/
+  MPI_Barrier(MPI_COMM_WORLD);
+
+  /* Get files into Hadoop and put them into HDFS*/
+  if (me == 0) {
+    MasterPutHDFS();
+  }
+
+  MPI_Finalize();
+}
