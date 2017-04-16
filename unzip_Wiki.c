21c6797394f0821a5e49e5a405987005719be496
Larry Li <li_j8@denison.edu>
2017-04-16 14:08:38 -0400

    unzipping 2015.06.01-2015.12.31 plus track the missing .gz files

--- unzip_Wiki.c
+++ unzip_Wiki.c
@@ -1,7 +1,8 @@
 // Description: This file works for downloading the data from wikimedia traffic data by using MPI
-//mpicc -g -o getWiki_data getWiki_data.c
-//mpiexec -f hosts -n 16 ./getWiki_data
-
+/*
+gcc -o unzip_Wiki unzip_Wiki.c -std=c99
+./unzip_Wiki
+*/
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -19,85 +20,97 @@ int main() {
     char year[4];
     char month[2];
 
-    char nameOfGZ[26];
-    char URL_forDay[82];
-    char Download_gz_URL[100];
-
+    // Used in this file
     char Unzip[200];
     char Delete_gz[200];
+    char Test_exist[200];
 
-    char MakeDir[28];
-    char wgetCommand[122];
-    //char unZipCommand[114];
-    //char RemoveCommand[114];
+    FILE *fptr;
+    fptr = fopen("missing_files.txt", "w");
+    if (fptr == NULL)
+    {
+      printf("Error!\n");
+      exit(1);
+    }
 
-    int start = 1;
-    int end = 245;
 
-    //system("mkdir /tmp/WikiData");  //Create WikiData directory
-    //584
+    int cmd;
 
       //year = 0 -> 1900
       //mon = 0 -> 1
       //day = 1 -> 1
+      //Set up the start date
       ptm->tm_year = 115; //2015
-      ptm->tm_mon = 0;
-      ptm->tm_mday = 1;
+      ptm->tm_mon = 5; // 6
+      ptm->tm_mday = 1; // 1
       mktime(ptm);
       strftime(date, 9, "%Y%m%d", ptm);
+      printf("date: %s\n", date);
       memcpy(&year, &date[0], 4);
       year[4] = '\0';
       memcpy(&month, &date[4], 2);
       month[2] = '\0';
 
-      for (int i = 1; i <= 365; i++)
+      for (int i = 1; i <= 214; i++)
       {
         for (int j = 0; j <= 23; j++)
         {
           if (j <= 9)
           {
-            snprintf(Unzip, 200 ,"hadoop fs -text /user/valdiv_n1/Wiki_Data/WikiData/%s-%s/pagecounts-%s-0%d0000.gz | hadoop fs -put - /user/valdiv_n1/Wiki_Data/WikiData/%s-%s/pagecounts-%s-0%d0000.gz", year, month, date, j, year, month, date, j);
+            // Used for testing. Unzip from li_j8's dirctory to li_j8's dictory
+            // snprintf(Unzip, 200 ,"hadoop fs -text ./Wiki/%s-%s/pagecounts-%s-0%d0000.gz | hadoop fs -put - ./Wiki/%s-%s/pagecounts-%s-0%d0000.txt", year, month, date, j, year, month, date, j);
+            // snprintf(Delete_gz, 200 ,"hdfs dfs -rm ./Wiki/%s-%s/pagecounts-%s-0%d0000.gz", year, month, date, j);
+
+            // Used for actually unzipping. Unzip from valdiv_n1's dirctory to li_j8's dictory
+            snprintf(Unzip, 200 ,"hadoop fs -text /user/valdiv_n1/Wiki_Data/WikiData/%s-%s/pagecounts-%s-0%d0000.gz | hadoop fs -put - ./Wiki/%s-%s/pagecounts-%s-0%d0000.txt", year, month, date, j, year, month, date, j);
             snprintf(Delete_gz, 200 ,"hdfs dfs -rm /user/valdiv_n1/Wiki_Data/WikiData/%s-%s/pagecounts-%s-0%d0000.gz", year, month, date, j);
           }
           else
           {
-            snprintf(Unzip, 200 ,"hadoop fs -text /user/valdiv_n1/Wiki_Data/WikiData/%s-%s/pagecounts-%s-%d0000.gz | hadoop fs -put - /user/valdiv_n1/Wiki_Data/WikiData/%s-%s/pagecounts-%s-%d0000.gz", year, month, date, j, year, month, date, j);
-            snprintf(Delete_gz, 200 ,"hdfs dfs -rm /user/valdiv_n1/Wiki_Data/WikiData/%s-%s/pagecounts-%s-0%d0000.gz", year, month, date, j);
+            // snprintf(Unzip, 200 ,"hadoop fs -text ./Wiki/%s-%s/pagecounts-%s-%d0000.gz | hadoop fs -put - ./Wiki/%s-%s/pagecounts-%s-%d0000.txt", year, month, date, j, year, month, date, j);
+            // snprintf(Delete_gz, 200 ,"hdfs dfs -rm ./Wiki/%s-%s/pagecounts-%s-%d0000.gz", year, month, date, j);
+
+            // Used for actually unzipping. Unzip from valdiv_n1's dirctory to li_j8's dictory
+            snprintf(Unzip, 200 ,"hadoop fs -text /user/valdiv_n1/Wiki_Data/WikiData/%s-%s/pagecounts-%s-%d0000.gz | hadoop fs -put - ./Wiki/%s-%s/pagecounts-%s-%d0000.txt", year, month, date, j, year, month, date, j);
+            snprintf(Delete_gz, 200 ,"hdfs dfs -rm /user/valdiv_n1/Wiki_Data/WikiData/%s-%s/pagecounts-%s-%d0000.gz", year, month, date, j);
           }
-          printf("%s\n", Unzip);
-          printf("%s\n", Delete_gz);
-          printf("\n");
+
+          if (j <= 9)
+          {
+            //-e: if the path exists, return 0.
+            snprintf(Test_exist, 200, "hdfs dfs -test -e /user/valdiv_n1/Wiki_Data/WikiData/%s-%s/pagecounts-%s-0%d0000.gz", year, month, date, j);
+            cmd = system(Test_exist);
+          }
+          else
+          {
+            //-e: if the path exists, return 0.
+            snprintf(Test_exist, 200, "hdfs dfs -test -e /user/valdiv_n1/Wiki_Data/WikiData/%s-%s/pagecounts-%s-%d0000.gz", year, month, date, j);
+            cmd = system(Test_exist);
+          }
+          if (cmd == 0)
+          {
+            cmd = system(Unzip);
+            cmd = system(Delete_gz);
+            printf("%s\n", Unzip);
+            printf("%s\n", Delete_gz);
+            printf("\n");
+          }
+          else
+          {
+            printf("Missing date: %s-%d\n", date, j);
+            fprintf(fptr, "Missing date: %s-%d\n", date, j);
+          }
         }
         ptm->tm_mday += 1;
         mktime(ptm);
         strftime(date, 9, "%Y%m%d", ptm);
+        // printf("date: %s\n", date);
         memcpy(&year, &date[0], 4);
         year[4] = '\0';
         memcpy(&month, &date[4], 2);
         month[2] = '\0';
       }
 
-      // snprintf(URL_forDay, 82, "https://dumps.wikimedia.org/other/pagecounts-raw/%s/%s-%s/pagecounts-%s", year, year, month, date);
-      // snprintf(MakeDir, 28, "mkdir /tmp/WikiData/%s-%s", year, month);
-
-      //system(MakeDir);
-
-
-//       for (int j = 0; j < 24; j++) {
-//         if (j < 10) {
-//           snprintf(Download_gz_URL, 100, "%s-0%d0000.gz", URL_forDay, j);
-//         }
-//         else {
-//           snprintf(Download_gz_URL, 100, "%s-%d0000.gz", URL_forDay, j);
-//         }
-//         printf("%s\n", Download_gz_URL);/*
-//         memcpy(&nameOfGZ, &Download_gz_URL[62], 26);
-//         nameOfGZ[26] = '\0';  //get name of the
-//
-//         snprintf(wgetCommand, 154, "wget %s -P /tmp/WikiData/%s-%s", Download_gz_URL, year, month);
-//         system(wgetCommand);
-// */
-//       }
-
+    fclose(fptr);
     return 0;
 }
