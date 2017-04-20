8a1202dc61d8a6ed1ae62b730854a33834a1d4ba
Larry Li <li_j8@denison.edu>
2017-04-20 19:10:35 -0400

    add missing dates

--- CURATE_DAY.c
+++ CURATE_DAY.c
@@ -18,15 +18,12 @@ void PutHDFS()
   cmd = system("rm -r /tmp/WikiData");
 }
 
-void workernode(int mon, int day, int st_hr)
+void workernode(int mon, int day, int st_hr, int end_hr)
 {
   int cmd, start, end, i, j;
   char date[9];
   char year[4];
   char month[2];
-  //start at 0153.11.9  --> 2007/12/09
-  //2015   --> 0163.0.1
-  //end at 0153.11.3171   -->2016/08/05
   time_t t = time(NULL);
   struct tm * ptm = gmtime(&t);
 
@@ -52,7 +49,7 @@ void workernode(int mon, int day, int st_hr)
 
   cmd = system(MakeDir);
 
-  for (j = st_hr; j < 24; j++) {
+  for (j = st_hr; j <= end_hr; j++) {
     if (j < 10) {
       snprintf(Download_gz_URL, 100, "%s-0%d0000.gz", URL_forDay, j);
     }
@@ -68,29 +65,16 @@ void workernode(int mon, int day, int st_hr)
 
 int main(int argc, char** argv)
 {
-  /*int month, day, start_hr, numOFDAYS;
-  if (argc == 4) {
-    month = atoi(argv[1]);
-    day = atoi(argv[2]);
-    start_hr = atoi(argv[3]);
-  }
-  else {
-    printf("Insufficient arguments:\n Correct unsage: ./CURATE_DAY <month> <day> <starting hour>");
-    exit(-1);
-  }
-  workernode(month, day, start_hr);*/
-  workernode(8, 2, 5);
+  workernode(8, 17, 6, 23);
   PutHDFS();
-  workernode(8, 18, 6);
+  workernode(8, 18, 0, 5);
   PutHDFS();
-  workernode(9, 1, 9);
+  workernode(10,28, 0, 23);
   PutHDFS();
-  workernode(10,16, 5);
+  workernode(10, 29, 0, 23);
   PutHDFS();
-  workernode(11, 15, 4);
+  workernode(10, 30, 0, 23);
   PutHDFS();
-  workernode(11, 30, 9);
-  PutHDFS();
-  workernode(12, 15, 7);
+  workernode(10, 31, 0, 23);
   PutHDFS();
 }
